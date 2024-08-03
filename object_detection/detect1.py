28e5a1fb2851b3ad71a9a28e9d21504230082853
B.G.Mathivathani <vathanimathi124@gmail.com>
2024-08-03 11:00:10 +0530

    Add files via upload

---
+++ object_detection/detect1.py
@@ -0,0 +1,71 @@
+from torchvision import transforms
+from utils import *
+from PIL import Image, ImageDraw, ImageFont
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+torch.nn.Module.dump_patches = True
+checkpoint = 'checkpoint_ssd300.pth.tar'
+checkpoint = torch.load(checkpoint, map_location=torch.device('cpu'))
+start_epoch = checkpoint['epoch'] + 1
+print('\nLoaded checkpoint from epoch %d.\n' % start_epoch)
+model = checkpoint['model']
+model = model.to(device)
+model.eval()
+
+resize = transforms.Resize((300, 300))
+to_tensor = transforms.ToTensor()
+normalize = transforms.Normalize(
+    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+
+def detect(original_image, min_score, max_overlap, top_k, suppress=None):
+    image = normalize(to_tensor(resize(original_image)))
+    image = image.to(device)
+
+    predicted_locs, predicted_scores = model(image.unsqueeze(0))
+
+    det_boxes, det_labels, det_scores = model.detect_objects(predicted_locs, predicted_scores, min_score=min_score,
+                                                             max_overlap=max_overlap, top_k=top_k)
+
+    det_boxes = det_boxes[0].to('cpu')
+    original_dims = torch.FloatTensor(
+        [original_image.width, original_image.height, original_image.width, original_image.height]).unsqueeze(0)
+    det_boxes = det_boxes * original_dims
+
+    det_labels = [rev_label_map[l] for l in det_labels[0].to('cpu').tolist()]
+
+    if det_labels == ['background']:
+        return original_image
+
+    annotated_image = original_image.copy()
+    draw = ImageDraw.Draw(annotated_image)
+    font = ImageFont.truetype("./calibril.ttf", 15)
+
+    for i in range(det_boxes.size(0)):
+        if suppress is not None:
+            if det_labels[i] in suppress:
+                continue
+
+        box_location = det_boxes[i].tolist()
+        draw.rectangle(xy=box_location, outline=label_color_map[det_labels[i]])
+        draw.rectangle(xy=[l + 1. for l in box_location],
+                       outline=label_color_map[det_labels[i]])
+
+        text_size = draw.textbbox((0, 0), det_labels[i].upper(), font=font)
+        text_location = [box_location[0] + 2., box_location[1] - text_size[1]]
+        textbox_location = [box_location[0], box_location[1] - text_size[1],
+                            box_location[0] + text_size[0] + 4., box_location[1]]
+        draw.rectangle(xy=textbox_location,
+                       fill=label_color_map[det_labels[i]])
+        draw.text(xy=text_location,
+                  text=det_labels[i].upper(), fill='white', font=font)
+
+    del draw
+    return annotated_image
+
+
+if __name__ == '__main__':
+    img_path = '9.jpg'
+    original_image = Image.open(img_path).convert('RGB')
+    detect(original_image, min_score=0.2, max_overlap=0.5, top_k=200).show()
